52f9a2a79856f69a9eefb2364483c36887be23cc
WANG Ji <ijingobravo@gmail.com>
2016-10-26 21:30:11 +0800

    smallbank full version without init

--- smallbank/smallbank.go
+++ smallbank/smallbank.go
@@ -24,27 +24,26 @@ func main() {
 }
 
 func (t *SmallBank) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	/*
-		for i := 0; i < MAX_ACCOUNTS; i++ {
-			stub.PutState(accountTab+"_"+strconv.Itoa(i), []byte("name_"+strconv.Itoa(i)))
-			stub.PutState(savingTab+"_"+strconv.Itoa(i), []byte(strconv.Itoa(BALANCE)))
-			stub.PutState(checkingTab+"_"+strconv.Itoa(i), []byte(strconv.Itoa(BALANCE)))
-		}
-	*/
 
 	return nil, nil
 }
 
 func (t *SmallBank) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 
-	/*
-		if function == "updateBalance" {
-			return t.updateBalance(stub, args)
-		} else {
-			return nil, nil
-		}
+	if function == "amalgate" {
+		return t.almagate(stub, args)
+	} else if function == "getBalance" {
+		return t.getBalance(stub, args)
+	} else if function == "updateBalance" {
+		return t.updateBalance(stub, args)
+	} else if function == "updateSaving" {
+		return t.updateSaving(stub, args)
+	} else if function == "sendPayment" {
+		return t.sendPayment(stub, args)
+	} else if function == "writeCheck" {
+		return t.writeCheck(stub, args)
+	}
 
-	*/
 	return nil, nil
 
 }
@@ -53,15 +52,216 @@ func (t *SmallBank) Query(stub shim.ChaincodeStubInterface, function string, arg
 	return valAsbytes, err
 }
 
-func (t *SmallBank) updateBalance(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	stub.GetState(accountTab + "_" + args[0])
-	bal_str, err := stub.GetState(checkingTab + "_" + args[0])
+func (t *SmallBank) almagate(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	bal_str1, err1 := stub.GetState(accountTab + "_" + args[0])
+	if err1 != nil {
+		stub.PutState(accountTab+"_"+args[0], []byte([]byte(strconv.Itoa(BALANCE))))
+	}
+	bal_str2, err2 := stub.GetState(checkingTab + "_" + args[0])
+	if err2 != nil {
+		stub.PutState(checkingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	_, err3 := stub.GetState(savingTab + "_" + args[0])
+	if err3 != nil {
+		stub.PutState(savingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	bal_str1, err1 = stub.GetState(accountTab + "_" + args[1])
+	if err1 != nil {
+		stub.PutState(accountTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	bal_str2, err2 = stub.GetState(checkingTab + "_" + args[1])
+	if err2 != nil {
+		stub.PutState(checkingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	_, err3 = stub.GetState(savingTab + "_" + args[0])
+	if err3 != nil {
+		stub.PutState(savingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+
 	var bal1, bal2 int
+	var err error
+	bal_str1, err = stub.GetState(savingTab + "_" + args[0])
+	bal_str2, err = stub.GetState(checkingTab + "_" + args[1])
+
+	bal1, err = strconv.Atoi(string(bal_str1))
+	bal2, err = strconv.Atoi(string(bal_str2))
+	bal1 += bal2
+
+	err = stub.PutState(checkingTab+"_"+args[0], []byte(strconv.Itoa(0)))
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = stub.PutState(savingTab+"_"+args[1], []byte(strconv.Itoa(bal1)))
+
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func (t *SmallBank) getBalance(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	bal_str1, err1 := stub.GetState(accountTab + "_" + args[0])
+	if err1 != nil {
+		stub.PutState(accountTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	bal_str2, err2 := stub.GetState(checkingTab + "_" + args[0])
+	if err2 != nil {
+		stub.PutState(checkingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	_, err3 := stub.GetState(savingTab + "_" + args[0])
+	if err3 != nil {
+		stub.PutState(savingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+
+	var bal1, bal2 int
+	var err error
+	bal_str1, err = stub.GetState(savingTab + "_" + args[0])
+	bal_str2, err = stub.GetState(checkingTab + "_" + args[0])
+
+	bal1, err = strconv.Atoi(string(bal_str1))
+	bal2, err = strconv.Atoi(string(bal_str2))
+	bal1 += bal2
+
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func (t *SmallBank) updateBalance(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	_, err1 := stub.GetState(accountTab + "_" + args[0])
+	if err1 != nil {
+		stub.PutState(accountTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	bal_str, err2 := stub.GetState(checkingTab + "_" + args[0])
+	if err2 != nil {
+		stub.PutState(checkingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	_, err3 := stub.GetState(savingTab + "_" + args[0])
+	if err3 != nil {
+		stub.PutState(savingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	var bal1, bal2 int
+	var err error
 	bal1, err = strconv.Atoi(string(bal_str))
 	bal2, err = strconv.Atoi(args[1])
 	bal1 += bal2
 
 	err = stub.PutState(checkingTab+"_"+args[0], []byte(strconv.Itoa(bal1)))
+
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func (t *SmallBank) updateSaving(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	_, err1 := stub.GetState(accountTab + "_" + args[0])
+	if err1 != nil {
+		stub.PutState(accountTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	_, err2 := stub.GetState(checkingTab + "_" + args[0])
+	if err2 != nil {
+		stub.PutState(checkingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	bal_str3, err3 := stub.GetState(savingTab + "_" + args[0])
+	if err3 != nil {
+		stub.PutState(savingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	var bal1, bal2 int
+	var err error
+
+	bal1, err = strconv.Atoi(string(bal_str3))
+	bal2, err = strconv.Atoi(args[1])
+	bal1 += bal2
+
+	err = stub.PutState(savingTab+"_"+args[0], []byte(strconv.Itoa(bal1)))
+
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func (t *SmallBank) sendPayment(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	bal_str1, err1 := stub.GetState(accountTab + "_" + args[0])
+	if err1 != nil {
+		stub.PutState(accountTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	bal_str2, err2 := stub.GetState(checkingTab + "_" + args[0])
+	if err2 != nil {
+		stub.PutState(checkingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	_, err3 := stub.GetState(savingTab + "_" + args[0])
+	if err3 != nil {
+		stub.PutState(savingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	bal_str1, err1 = stub.GetState(accountTab + "_" + args[1])
+	if err1 != nil {
+		stub.PutState(accountTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	bal_str2, err2 = stub.GetState(checkingTab + "_" + args[1])
+	if err2 != nil {
+		stub.PutState(checkingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	_, err3 = stub.GetState(savingTab + "_" + args[0])
+	if err3 != nil {
+		stub.PutState(savingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+
+	var bal1, bal2, amount int
+	var err error
+
+	bal_str1, err = stub.GetState(checkingTab + "_" + args[0])
+	bal_str2, err = stub.GetState(checkingTab + "_" + args[1])
+	amount, err = strconv.Atoi(args[2])
+
+	bal1, err = strconv.Atoi(string(bal_str1))
+	bal2, err = strconv.Atoi(string(bal_str2))
+	bal1 -= amount
+	bal2 += amount
+
+	err = stub.PutState(checkingTab+"_"+args[0], []byte(strconv.Itoa(bal1)))
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = stub.PutState(checkingTab+"_"+args[1], []byte(strconv.Itoa(bal2)))
+
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func (t *SmallBank) writeCheck(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	_, err1 := stub.GetState(accountTab + "_" + args[0])
+	if err1 != nil {
+		stub.PutState(accountTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	bal_str2, err2 := stub.GetState(checkingTab + "_" + args[0])
+	if err2 != nil {
+		stub.PutState(checkingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+	bal_str3, err3 := stub.GetState(savingTab + "_" + args[0])
+	if err3 != nil {
+		stub.PutState(savingTab+"_"+args[0], []byte(strconv.Itoa(BALANCE)))
+	}
+
+	var bal1, bal2 int
+	var err error
+	var amount int
+	bal1, err = strconv.Atoi(string(bal_str2))
+	bal2, err = strconv.Atoi(string(bal_str3))
+	amount, err = strconv.Atoi(args[1])
+	if amount < bal1+bal2 {
+		err = stub.PutState(checkingTab+"_"+args[0], []byte(strconv.Itoa(bal1-amount-1)))
+	} else {
+		err = stub.PutState(checkingTab+"_"+args[0], []byte(strconv.Itoa(bal1-amount)))
+	}
 
 	if err != nil {
 		return nil, err
